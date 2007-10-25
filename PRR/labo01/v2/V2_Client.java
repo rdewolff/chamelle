a569dfdab89dbeee6e7468e69d7553a2e706d8e7
rdewolff <rdewolff@9b675267-343c-0410-a270-3319a1321a2a>
2007-10-25 11:03:16 +0000

    updates backup ^^

--- PRR/labo01/v2/V2_Client.java
+++ PRR/labo01/v2/V2_Client.java
@@ -3,32 +3,51 @@ package v2;
 import java.net.*;
 import java.io.*;
 
-public class V2_Client
-{
-	public static void main (String args[]) throws IOException
-	{
-		String message = "Allo";
-		byte[] tampon = new byte[256];
-		
-		// creation du message a envoyer au serveur
-		tampon = message.getBytes();
-		InetAddress address = InetAddress.getByName("localhost");
-		DatagramPacket paquet = new DatagramPacket(tampon, tampon.length, address, 4445);
-		
-		// obtenir un socket de datagramme
-		DatagramSocket socket = new DatagramSocket();
-		// envoyer le message
-		socket.send(paquet);
-		
-		// obtenir l'echo
-		DatagramPacket packet = new DatagramPacket(tampon, tampon.length);
-		socket.receive(packet);
-		String msg = new String(packet.getData(), 0);
-		
-		System.out.println("Paquet recu:" + msg);
-		
-		socket.close();
-		
+public class V2_Client {	
+	public static void main (String args[]) throws IOException {
+		try {
+			// exemple de donnée
+			String message = "caca";
+			byte[] tampon = new byte[256];
+			tampon = message.getBytes();
+			
+			// introduction
+			// TODO : attente de pression clavier
+			//System.out.println("Appuyer sur une touche pour continuer .. ")
+			// parametre de la connexion
+			InetAddress address = InetAddress.getByName("localhost");
+			int port = 4445;
+			// envoie au serveur un paquet de requete, pour qu'il sache qu'il est connecté
+			
+			// recoit la valeur de N
+			DatagramSocket socket = new DatagramSocket(port);
+			DatagramPacket packet = new DatagramPacket(tampon, tampon.length);
+			socket.receive(packet);
+			
+			int N = Integer.parseInt(packet.getData().toString()); // TODO : inutile ?
+			// obtenir un socket de datagramme
+			System.out.println("Valeur de N : " + N + "\n");
+			
+			// envoyer le message
+			socket.receive(packet);
+
+			String msg = new String(packet.getData(), 0);
+
+			
+			System.out.println("Paquet recu:" + msg);
+			
+			// recoit une ligne, un numéro de ligne et la matrice B
+			// calcul les valeurs
+			// renvoie les résultats au serveur
+			DatagramPacket paquet = new DatagramPacket(tampon, tampon.length, address, port);
+			socket.send(paquet);
+
+			// ferme la connection
+			socket.close();
+
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 	}
 }
 
--- PRR/labo01/v2/V2_Serveur.java
+++ PRR/labo01/v2/V2_Serveur.java
@@ -7,21 +7,33 @@ public class V2_Serveur
 {
 	public static void main (String args[]) throws IOException, SocketException 
 	{
+		
+		Integer N = 3;
+		
 		byte[] tampon = new byte[256];
 		
 		DatagramSocket socket = new DatagramSocket(4445);
 		
 		DatagramPacket paquet = new DatagramPacket(tampon, tampon.length);
-		socket.receive(paquet);
 		
-		String messageRecu = new String(paquet.getData(), 0);
-		System.out.println("Echo: " + messageRecu);
+		//socket.receive(paquet); // bloquant
 		
-		InetAddress addresseClient = paquet.getAddress();
-		int portClient = paquet.getPort();
+		//String messageRecu = new String(paquet.getData(), 0);
+		//System.out.println("Echo: " + messageRecu);
 		
-		tampon = messageRecu.getBytes();
-		paquet = new DatagramPacket(tampon, tampon.length, addresseClient, portClient);
+		// 1 - InetAddress addresseClient = paquet.getAddress();
+		// 2 - 
+		InetAddress addresseClient = InetAddress.getByName("localhost");
+		//int portClient = paquet.getPort();
+		
+		// 1 - tampon = messageRecu.getBytes();
+		// 2 -
+		tampon[0] = N.byteValue();
+		
+		
+		// 1 - paquet = new DatagramPacket(tampon, tampon.length, addresseClient, portClient);
+		// 2 -
+		paquet = new DatagramPacket(tampon, tampon.length, addresseClient, 4445);
 		
 		socket.send(paquet);
 		
