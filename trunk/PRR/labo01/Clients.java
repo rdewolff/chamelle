0bb9431ea72aee509034c1705192752c204fc8ae
rdewolff <rdewolff@9b675267-343c-0410-a270-3319a1321a2a>
2007-11-09 15:57:34 +0000

    Commentaires

--- trunk/PRR/labo01/Clients.java
+++ trunk/PRR/labo01/Clients.java
@@ -1,5 +1,12 @@
 import java.net.*;
 
+/**
+ * Classe qui permet de stocker les informations sur les clients connecté
+ * 
+ * @author Romain de Wolff
+ * @author Simon Hintermann
+ */
+
 public class Clients {
 
 	static int nbClient = 0;
@@ -8,16 +15,31 @@ public class Clients {
 	private InetAddress adr;
 	private int port;
 	
+	/**
+	 * Constructeur 
+	 * @param id	le numero du client
+	 * @param adr	l'adresse internet du client
+	 * @param port	le port du client
+	 */
 	public Clients (int id, InetAddress adr, int port) {
+		this.nbClient++; // inutilisé dans cette version
 		this.id = id;
 		this.adr = adr;
 		this.port = port;
 	}
 	
+	/**
+	 * Retourne l'adresse du client
+	 * @return	l'adresse du client 
+	 */
 	public InetAddress getAddress() {
 		return this.adr;
 	}
 	
+	/**
+	 * Retourne le port du client 
+	 * @return	le port du client
+	 */
 	public int getPort() {
 		return this.port;
 	}
