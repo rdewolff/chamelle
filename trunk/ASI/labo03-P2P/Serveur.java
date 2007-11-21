980da26db8b9e478d47133c7e43520b6c3a1a296
rdewolff <rdewolff@9b675267-343c-0410-a270-3319a1321a2a>
2007-11-21 08:11:59 +0000

    Création de la structure du laboratoire de P2P

---
+++ trunk/ASI/labo03-P2P/Serveur.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author rdewolff
+ *
+ */
+
+public class Serveur {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
