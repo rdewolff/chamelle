464743d4c51b5c06f1058764c2f2ef985a759421
rdewolff <rdewolff@9b675267-343c-0410-a270-3319a1321a2a>
2007-11-21 08:11:59 +0000

    Création de la structure du laboratoire de P2P

---
+++ ASI/labo03-P2P/NoeudAmorcage.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author rdewolff
+ *
+ */
+
+public class NoeudAmorcage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
